7198c5a441f45da5cdcb5f92f55bc9edea095fea
TonhitoMS <antonio.moscoso.sanchez@gmail.com>
2021-10-10 23:04:11 +0200

    Código final para entregar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#Antonio Moscoso Sánchez
+#TALF Grupo 2
+
 import sys
 
-from tabulate import tabulate
 
 estados_totais = []
 estados_finais = []
@@ -29,27 +31,16 @@ def ProcesarTexto():
 #Método que realiza as transcións coa cadea baleira dun conxunto de estados dado
 def Clausura(estadosE, Transicions, estados_totais, simbolos):
     estadosRes = []
-    #print("Clausura")
-    #print(estadosE)
-    #print(len(estadosE))
     for j in range(0, len(estadosE)):
-        #print("Clausura de")
-        #print(estadosE[j])
         e = Transicions[estados_totais.index(estadosE[j])][len(simbolos)]
-        #print(e == "  ")
         if not (e == " " or e == "" or e == "  "):
             if e.find(" ") >= 0:
                 x = e.split(" ")
-                #print(x)
-                #print(len(x))
                 for k in range(0, len(x)):
                     if not x[k] == "":
                         estadosRes.append(x[k])
-                        #print(x[k])
             estadosRes.append(estadosE[j])
-            #print(e)
         else:
-            #print("Non ten clausura")
             estadosRes.append(estadosE[j])
     estadosRes = list(set(estadosRes))
     estadosRes.sort()
@@ -64,13 +55,7 @@ def estadoFinal():
 
 
 estados_finais, estados_totais, simbolos = ProcesarTexto()
-print(tabulate(Transicions))
-print(estados_finais)
-print(estados_totais)
-print(simbolos)
-#print(len(Transicions[0]))
 max = len(Transicions[0])
-#print(max)
 for j in Transicions:
     del j[max-1]
 print(Transicions[0])
@@ -78,11 +63,6 @@ print(Transicions[0])
 estadoAct = []
 estadoC = []
 estadoAct.append(estados_totais[0])
-# estadoAct = ['A', 'B']
-# print(estadoAct)
-# estadoAct = Clausura(estadoAct, Transicions, estados_totais, simbolos)
-# print(estadoAct)
-#estadoAct = Clausura(estadoAct, Transicions, estados_totais, simbolos)
 
 #clausura do estado inicial
 while estadoAct != estadoC:
@@ -101,41 +81,26 @@ for i in sys.argv[2]:
     v.append(i)
     print(estadoAct)
     estadoAux = []
-    # while estadoAct != estadoC:
-    #     estadoAct = Clausura(estadoAct, Transicions, estados_totais, simbolos)
-    #     estadoC = Clausura(estadoAct, Transicions, estados_totais, simbolos)
-    # print("Estados Clausura:")
-    # print(estadoAct)
 
     for j in range(0, len(estadoAct)):
-        #print("j = " + str(j))
-        #print("Estado actual " + estadoAct[j])
         e = Transicions[estados_totais.index(estadoAct[j])][simbolos.index(i)]
         if e.find(" ") >= 0:
-            #print("varios estados")
             x = e.split(" ")
-            #print("Estados: ")
-            #print(x)
             for w in range(0, len(x)):
                 if not x[w] == "":
                     estadoAux.append(x[w])
         else:
             if not e == "":
                 estadoAux.append(e)
-        #print("Estado actual")
-        #print(estadoAct)
     estadoAct = estadoAux
     while estadoAct != estadoC:
         estadoAct = Clausura(estadoAct, Transicions, estados_totais, simbolos)
         estadoC = Clausura(estadoAct, Transicions, estados_totais, simbolos)
-    # print("Estados Clausura:")
-    # print(estadoAct)
     print("Estado final da iteracción")
     print(estadoAct)
     estadoFinal()
     print("********************************\n")
 
-#estadoAct = Clausura(estadoAct, Transicions, estados_totais, simbolos)
 estadoAct = list(set(estadoAct))
 estadoAct.sort()
 
